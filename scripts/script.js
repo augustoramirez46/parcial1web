355b53170a2e7c01c850f6b1f583de11347e1405
augustoramirez46 <tutoead46@gmail.com>
2020-03-13 08:26:59 -0500

    yayes

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,11 @@
-var rotationInput = document.querySelector('.parc1__rotation');
-var rotationImage = document.querySelector('.parc1__rotation');
+var rotationInput = document.querySelector('.parc1__range');
+var rotationImage = document.querySelector('.parc1__img');
+
+function handleRotationInput() {
+    var index = rotationInput.value;
+    // rotationImage.setAttribute('transform', 'rotate(' + index + 'deg);');
+    rotationImage.style.transform = ('rotate(' + index + 'deg)');
+}
+
+handleRotationInput();
+rotationInput.addEventListener('input', handleRotationInput);
